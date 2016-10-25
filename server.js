2a53708765194e017566e657dce2e7c5d07e59d3
alex rodriguez <alex.rodriguez@austintexas.gov>
2016-10-25 11:44:43 -0500

    add underscore, basic validation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -40,27 +41,44 @@ app.get('/todos', function(req, res) {
 });
 
 // get single todo - GET /todos/:id
+// app.get('/todos/:id', function(req, res) {
+//   var todoId = parseInt(req.params.id, 10);
+//   var matchedTodo;
+//   todos.forEach(function (todo) {
+//     console.log(typeof todo.id);
+//     if(todoId === todo.id) {
+//       matchedTodo = todo;
+//     }
+//   });
+//   if(matchedTodo) {
+//     res.json(matchedTodo);
+//   } else {
+//     res.status(404).send();
+//   }
+// });
+//
+// doing the same as above but using underscore
 app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo;
-  todos.forEach(function (todo) {
-    console.log(typeof todo.id);
-    if(todoId === todo.id) {
-      matchedTodo = todo;
-    }
-  });
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+
   if(matchedTodo) {
     res.json(matchedTodo);
   } else {
     res.status(404).send();
   }
+
 });
 
 
 // POST /todos
 app.post('/todos', function(req, res) {
-  var body = req.body;
+  var body = _.pick(req.body, 'description', 'completed');
+  if( !_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
+    return res.status(404).send();
+  }
 
+  body.description = body.description.trim();
   body.id = todoNextId;
   todos.push(body);
   todoNextId++;

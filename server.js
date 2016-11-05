97e7e347e7f32784edb0a46924bd7debafdca9ae
alex rodriguez <alex.rodriguez@austintexas.gov>
2016-11-04 23:44:03 -0500

    update PUT /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -179,30 +179,62 @@ app.delete('/todos/:id', function(req, res) {
 
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
-  var body = _.pick(req.body, 'description', 'completed');
-  var validAttributes = {};
   var todoId = parseInt(req.params.id, 20);
-  var matchedTodo = _.findWhere(todos, {id: todoId});
+  var body = _.pick(req.body, 'description', 'completed');
+  var attributes = {};
 
-  if(!matchedTodo) {
-    return res.status(404).send();
+
+  if (body.hasOwnProperty('completed')) {
+    attributes.completed = body.completed;
   }
 
-  if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-    validAttributes.completed = body.completed;
-  } else if (body.hasOwnProperty('completed')) {
-    // not valid
-    return res.status(400).send();
+  if (body.hasOwnProperty('description')) {
+    attributes.description = body.description;
   }
 
-  if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.length > 0) {
-    validAttributes.description = body.description;
-  } else if(body.hasOwnProperty('description')) {
-    return res.status(400).send();
-  }
 
-  _.extend(matchedTodo, validAttributes);
-  res.json(matchedTodo);
+  // this is what is happening here:
+  // the first promise is to check that everything went right with findById. (2 promises)
+  // the second promise is a follow-up to todo.update
+  db.todo.findById(todoId).then(function (todo) {
+    if (todo) {
+      todo.update(attributes).then(function (todo) {
+        res.json(todo.toJSON());
+      }, function (e) {
+        res.status(400).json(e);
+      });
+    } else {
+      res.status(404).send();
+    }
+  }, function () {
+    res.status(500).send();
+  });
+
+
+  // var validAttributes = {};
+  // var todoId = parseInt(req.params.id, 20);
+
+  // var matchedTodo = _.findWhere(todos, {id: todoId});
+  //
+  // if(!matchedTodo) {
+  //   return res.status(404).send();
+  // }
+
+  // if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+  //   validAttributes.completed = body.completed;
+  // } else if (body.hasOwnProperty('completed')) {
+  //   // not valid
+  //   return res.status(400).send();
+  // }
+  //
+  // if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.length > 0) {
+  //   validAttributes.description = body.description;
+  // } else if(body.hasOwnProperty('description')) {
+  //   return res.status(400).send();
+  // }
+  //
+  // _.extend(matchedTodo, validAttributes);
+  // res.json(matchedTodo);
 
 
 

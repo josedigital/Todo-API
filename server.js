e1a74ad23559849ead640ceb86d0fdfec5dc0e40
alex rodriguez <alex.rodriguez@austintexas.gov>
2016-11-04 23:20:59 -0500

    convert delete todos to sequlize

--- server.js
+++ server.js
@@ -149,14 +149,28 @@ app.post('/todos', function(req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 20);
-  var matchedTodo = _.findWhere(todos, {id: todoId});
 
-  if(!matchedTodo) {
-    res.status(404).json({"error": "nothing found with that id"});
-  } else {
-    todos = _.without(todos, matchedTodo);
-    res.json(matchedTodo);
-  }
+  db.todo.destroy({
+    where: {
+      id: todoId
+    }
+  }).then(function (rowsDeleted) {
+    if(rowsDeleted === 0) {
+      res.json({error: 'no todo with that id'});
+    } else {
+      res.status(204).send();
+    }
+  }, function () {
+    res.status(500).send();
+  });
+  // var matchedTodo = _.findWhere(todos, {id: todoId});
+  //
+  // if(!matchedTodo) {
+  //   res.status(404).json({"error": "nothing found with that id"});
+  // } else {
+  //   todos = _.without(todos, matchedTodo);
+  //   res.json(matchedTodo);
+  // }
 
 });
 
